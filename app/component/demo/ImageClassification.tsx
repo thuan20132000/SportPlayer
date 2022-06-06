3f75b2a583c3d4f55d403290de40eede880b0749
ThuanHome <thuan20132000@gmail.com>
2022-06-06 21:07:51 +0700

    update

--- app/component/demo/ImageClassification.tsx
+++ app/component/demo/ImageClassification.tsx
@@ -3,55 +3,60 @@ import { Text, StyleSheet, View } from 'react-native';
 import { Camera, Image, MobileModel } from 'react-native-pytorch-core';
 
 
-const model = require("../../assets/mobilenet_v3_small.ptl");
-var json = JSON.parse(JSON.stringify(model));
+const model = require("../../assets/fashionMobileModel1.ptl");
+// var json = JSON.parse(JSON.stringify(model));
 
 
 type ImageClassificationResult = {
     maxIdx: number;
     confidence: number;
 };
-const ImageClasses = require('../../assets/MobileNetV3Classes.json');
+const ImageClasses = require('../../assets/MobileFashionClasses.json');
 
 
 export default function ImageClassificationDemo() {
     // Get safe area insets to account for notches, etc.
 
-
     // Component state that holds the detected object class
     const [objectClass, setObjectClass] = React.useState<string>('');
 
     async function handleImage(image: Image) {
-        const { result } = await MobileModel.execute<ImageClassificationResult>(
-            model,
-            {
-                image,
 
-            },
-        );
+        try {
+            console.log('image: ', image);
 
-        // Get max index (argmax) result to resolve the top class name
-        const topClass = ImageClasses[result.maxIdx];
+            const { result } = await MobileModel.execute<ImageClassificationResult>(
+                model,
+                {
+                    image,
 
-        // Log top class to Metro console
-        console.log(topClass);
+                },
+            )
 
-        // It is important to release the image to avoid memory leaks
-        image.release();
+            // Get max index (argmax) result to resolve the top class name
+            // const topClass = ImageClasses[result.maxIdx];
 
-        // if (result.confidence > 0.3) {
-        //     // Get max index (argmax) result to resolve the top class name
-        //     const topClass = ImageClasses[result.maxIdx];
+            // Log top class to Metro console
+            console.log('rs: ', ImageClasses[result.maxIdx]);
+            console.log('rs: ', result.confidence);
 
-        //     // Set object class state to be the top class detected in the image
-        //     setObjectClass(topClass);
-        // } else {
-        //     // Reset the object class if confidence value is low
-        //     setObjectClass('');
-        // }
+            // It is important to release the image to avoid memory leaks
+            image.release();
 
-        // It is important to release the image to avoid memory leaks
-        image.release();
+            if (result.confidence > 0.3) {
+                // Get max index (argmax) result to resolve the top class name
+                const topClass = ImageClasses[result.maxIdx];
+
+                // Set object class state to be the top class detected in the image
+                setObjectClass(topClass);
+            } else {
+                // Reset the object class if confidence value is low
+                setObjectClass('');
+            }
+        } catch (error) {
+            console.log('error:: ', error);
+        }
+
     }
 
 
@@ -59,8 +64,11 @@ export default function ImageClassificationDemo() {
         <View
             style={[
                 styles.container,
-
+                { marginTop: 60 },
             ]}>
+
+            <Text style={styles.label}>Object: {objectClass}</Text>
+
             <Text style={styles.label}>Image Classification</Text>
             <Camera style={styles.camera} onCapture={handleImage} />
         </View>
